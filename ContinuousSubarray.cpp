1641fd7a69612de7aeb866b7e86e568135cbbc33
surajhe <86972874+surajhe@users.noreply.github.com>
2021-07-08 19:31:24 +0530

    Create ContinuousSubarray.cpp

---
+++ ContinuousSubarray.cpp
@@ -0,0 +1,6 @@
+// Given an unsorted array A of size N of non- negative integers, find a continuous subarray which adds to a given number S.
+// Input:
+// N = 5, S = 12
+// A[] = {1,2,3,7,5}
+// Output = {2,3,7}
+
